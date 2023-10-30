fa354f7a4b43bc287758a85c798e30c31b43d198
kyw0300 <kyw0300@naver.com>
2023-10-30 23:19:45 +0900

    Feat:231030 data types, let vs var, hoisting

---
+++ variable.js
@@ -0,0 +1,116 @@
+// 1. Use strict
+// added in ES 5
+// use this for Vanila Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+
+{
+    let name = '정대만';
+    console.log(name);
+    name = "서태웅";
+    console.log(name);
+    console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+
+// console.log("age",age);
+{
+    age = 18;
+    var age;
+}
+console.log("age",age);
+
+// 3. Contants
+// favor immutable data type always for a few reasons:
+// security
+// thread safety(다양한 스레드들에 의해 동시에 값이 변경될 수 있는 위험 방지)
+// reduce human mintakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undifined, symbol
+// object, box container
+// function, first-class function
+
+// 숫자 타입을 지정하지 않아도 됨!
+// TS에서는 
+// let a: number = 12;
+// let a: number = 1.2;
+
+const count = 17;   // integer
+const size = 17.1;  // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - speicla numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet) 크롬, 사파리에서만 가능
+const bigInt = 1234567890123456789012345678901234567890n; // over (-2**53) ~ 2*53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+
+// string
+const char = 'c';
+const brendan = '강백호';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; //template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+// 우선순위를 주고싶을 때 사용
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+// .descrition으로 string으로 바꿔서 출력해야함
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// object, real-life object, data structure
+const jdm = { name: '정대만', age: 18 };
+jdm.age = 21;   // const type 이어도 속성 값들은 변경 가능
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));    // error: 타입이 숫자 타입으로 바뀌었음
