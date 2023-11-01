4427ef313be9a144071ccfd872c92a9805b441f3
kyw0300 <kyw0300@naver.com>
2023-11-01 21:56:54 +0900

    Feat:231101 operator 수정

--- operator.js
+++ operator.js
@@ -1,54 +1,26 @@
-// 1. String concatenation
-console.log('my' + ' cat');
-console.log('1' + 2);
+// 문자열 연산
+console.log('정' + '대만'); // 정대만
+console.log('1' + 2);       // 12
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
 
-// 2. Numeric operators
-console.log(1 + 1); // add
-console.log(1 - 1); // substract
-console.log(1 / 1); // divide
-console.log(1 * 1); // multiply
-console.log(5 % 2); // remainder
-console.log(2 ** 3); // exponentiation
-
-// 3. Increment and decrement operators
-let counter = 2;
-const preIncrement = ++counter;
-// counter = counter + 1;
-// preIncrement = counter;
-console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
-const postIncrement = counter++;
-// postIncrement = counter;
-// counter = counter + 1;
-console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
-const preDecrement = --counter;
-console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
-const postDecrement = counter--;
-console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
-
-// 4. Assignment operators
-let x = 3;
-let y = 6;
-x += y; // x = x + y;
-x -= y;
-x *= y;
-x /= y;
-
-// 5. Comparison operators
+// 비교 연산자(Comparison operators)
 console.log(10 < 6); // less than
 console.log(10 <= 6); // less than or equal
 console.log(10 > 6); // greater than
 console.log(10 >= 6); // greater than or equal
 
-// 6. Logical operators: || (or), && (and), ! (not)
+// 논리 연산자(Logical operators): ||(or), &&(and), !(not)
 const value1 = true;
 const value2 = 4 < 2;
 
 // || (or), finds the first truthy value
-console.log(`or: ${value1 || value2 || check()}`);
+console.log(`or: ${value1 || value2 || check()}`);  
+// 앞부분이 true이면 더 이상 진행되지 않음
+// 함수 호출같은 부분을 뒤로
 
 // && (and), finds the first falsy value
 console.log(`and: ${value1 && value2 && check()}`);
+// 앞부분이 false이면 더 이상 진행되지 않음
 
 // often used to compress long if-statement
 // nullableObject && nullableObject.something
@@ -56,121 +28,36 @@ console.log(`and: ${value1 && value2 && check()}`);
 function check() {
   for (let i = 0; i < 10; i++) {
     //wasting time
-    console.log('😱');
+    console.log('하하');
   }
   return true;
 }
 
-// ! (not)
-console.log(!value1);
-
-// 7. Equality
+// Equality
 const stringFive = '5';
 const numberFive = 5;
 
 // == loose equality, with type conversion
-console.log(stringFive == numberFive);
-console.log(stringFive != numberFive);
+console.log(stringFive == numberFive);    // true
+console.log(stringFive != numberFive);    // false
 
 // === strict equality, no type conversion
-console.log(stringFive === numberFive);
-console.log(stringFive !== numberFive);
+console.log(stringFive === numberFive);   // false
+console.log(stringFive !== numberFive);   // true
 
 // object equality by reference
-const ellie1 = { name: 'ellie' };
-const ellie2 = { name: 'ellie' };
-const ellie3 = ellie1;
-console.log(ellie1 == ellie2);
-console.log(ellie1 === ellie2);
-console.log(ellie1 === ellie3);
+const jdm1 = { name: '정대만' };
+const jdm2 = { name: '정대만' };
+const jdm3 = jdm1;
+console.log("jdm");
+console.log(jdm1 == jdm2);    // false
+console.log(jdm1 === jdm2);   // false
+console.log(jdm1 === jdm3);   // true
 
-// equality - puzzler
-console.log(0 == false);
-console.log(0 === false);
-console.log('' == false);
-console.log('' === false);
-console.log(null == undefined);
-console.log(null === undefined);
-
-// 8. Conditional operators: if
-// if, else if, else
-const name = 'df';
-if (name === 'ellie') {
-  console.log('Welcome, Ellie!');
-} else if (name === 'coder') {
-  console.log('You are amazing coder');
-} else {
-  console.log('unkwnon');
-}
-
-// 9. Ternary operator: ?
-// condition ? value1 : value2;
-console.log(name === 'ellie' ? 'yes' : 'no');
-
-// 10. Switch statement
-// use for multiple if checks
-// use for enum-like value check
-// use for multiple type checks in TS
-const browser = 'IE';
-switch (browser) {
-  case 'IE':
-    console.log('go away!');
-    break;
-  case 'Chrome':
-  case 'Firefox':
-    console.log('love you!');
-    break;
-  default:
-    console.log('same all!');
-    break;
-}
-
-// 11. Loops
-// while loop, while the condition is truthy,
-// body code is executed.
-let i = 3;
-while (i > 0) {
-  console.log(`while: ${i}`);
-  i--;
-}
-
-// do while loop, body code is executed first,
-// then check the condition.
-do {
-  console.log(`do while: ${i}`);
-  i--;
-} while (i > 0);
-
-// for loop, for(begin; condition; step)
-for (i = 3; i > 0; i--) {
-  console.log(`for: ${i}`);
-}
-
-for (let i = 3; i > 0; i = i - 2) {
-  // inline variable declaration
-  console.log(`inline variable for: ${i}`);
-}
-
-// nested loops
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
-    console.log(`i: ${i}, j:${j}`);
-  }
-}
-
-// break, continue
-// Q1. iterate from 0 to 10 and print only even numbers (use continue)
-for (let i = 0; i < 11; i++) {
-  if (i % 2 === 0) {
-    continue;
-  }
-  console.log(`q1. ${i}`);
-}
-
-// Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
-for (let i = 0; i < 11; i++) {
-  if (i > 8) {
-    break;
-  }
-  console.log(`q2. ${i}`);
-}
+// 퀴즈
+console.log(0 == false);          // true
+console.log(0 === false);         // false
+console.log('' == false);         // true
+console.log('' === false);        // false
+console.log(null == undefined);   // true
+console.log(null === undefined);  // false
