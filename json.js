1b5d2ea279a32b8b0fa823549303f03bae64ec60
kyw0300 <kyw0300@naver.com>
2023-11-08 21:57:05 +0900

    Feat:231108 json.js 추가

---
+++ json.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// JSON
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: function () {
+        console.log(`${this.name} can jump!`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log("rabbit", json);
+
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
+
+// 좀 더 세밀하게 통제하고 싶을 때 사용
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'jeongdaeman' : value;
+});
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
+json = JSON.stringify(rabbit);
+console.log(json);
+// const obj = JSON.parse(json);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
